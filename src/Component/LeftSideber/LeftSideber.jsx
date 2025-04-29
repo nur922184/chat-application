932b77e95c6c5d3fea0140945c9d92882a5d4add
nur922184 <nuruzzaman.engbd@gmail.com>
2025-04-29 16:40:45 +0600

    update some

--- src/Component/LeftSideber/LeftSideber.jsx
+++ src/Component/LeftSideber/LeftSideber.jsx
@@ -3,11 +3,29 @@ import './LeftSideber.css'
 import assets from '../../assets/assets';
 const LeftSideber = () => {
     return (
-        <div  className='ls'>
+        <div className='ls'>
             <div className="ls-top">
                 <div className="ls-nav">
-                    <img src={assets} alt="" />
+                    <img src={assets.logo} className='logo' alt="" />
+                    <div className="menu">
+                        <img src={assets.menu_icon} alt="" />
+                    </div>
                 </div>
+                <div className="ls-search">
+                    <img src={assets.search_icon} alt="" />
+                    <input type="text" placeholder='Search hare' />
+                </div>
+            </div>
+            <div className="ls-list">
+                {Array(12).fill("").map((item, index ) => (
+                    <div key={index} className="friends">
+                        <img src={assets.profile_img} alt="" />
+                        <div>
+                            <p>Richard Sanford</p>
+                            <span>Hello, How are You?</span>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     );
