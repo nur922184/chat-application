55d33350bf97276e7710a4fbce3c449179f34c94
nur922184 <nuruzzaman.engbd@gmail.com>
2025-05-04 21:35:07 +0600

    update the chatbox top section

--- src/Component/ChatBox/ChatBox.jsx
+++ src/Component/ChatBox/ChatBox.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './ChatBox.css'
+import assets from '../../assets/assets';
 const ChatBox = () => {
     return (
-        <div>
-            
+        <div className='chat-box'>
+            <div className="chat-user">
+                <img src={assets.profile_img} alt="" />
+                <p>Richard Sanford <img className='dot' src={assets.green_dot} alt="" /></p>
+                <img src={assets.help_icon} className='help' alt="" />
+            </div>
         </div>
     );
 };
