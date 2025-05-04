77bac1210dacd66155ea4a07192ba2695659172a
nur922184 <nuruzzaman.engbd@gmail.com>
2025-05-04 21:13:37 +0600

    ipdate the LeftSideBer

--- src/Component/LeftSideber/LeftSideber.jsx
+++ src/Component/LeftSideber/LeftSideber.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import './LeftSideber.css'
+import './LeftSideber.css';
 import assets from '../../assets/assets';
+
 const LeftSideber = () => {
     return (
         <div className='ls'>
             <div className="ls-top">
                 <div className="ls-nav">
-                    <img src={assets.logo} className='logo' alt="" />
+                    <img src={assets.logo} className='logo' alt="logo" />
                     <div className="menu">
-                        <img src={assets.menu_icon} alt="" />
+                        <img src={assets.menu_icon} alt="menu icon" />
                     </div>
                 </div>
                 <div className="ls-search">
-                    <img src={assets.search_icon} alt="" />
-                    <input type="text" placeholder='Search hare' />
+                    <img src={assets.search_icon} alt="search icon" />
+                    <input type="text" placeholder='Search here' />
                 </div>
             </div>
             <div className="ls-list">
-                {Array(12).fill("").map((item, index ) => (
+                {Array(12).fill("").map((_, index) => (
                     <div key={index} className="friends">
-                        <img src={assets.profile_img} alt="" />
+                        <img src={assets.profile_img} alt="profile" />
                         <div>
                             <p>Richard Sanford</p>
                             <span>Hello, How are You?</span>
